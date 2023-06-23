c6a7f554394ecf83888aa8bc1698223548afa5b2
Kritikaadhikarii <itskritikaaa@gmail.com>
2023-06-23 08:22:16 +0545

    new changes made

--- script.js
+++ script.js
@@ -13,28 +13,27 @@ buttons[0].textContent= "Rock";
 buttons[1].textContent= "Paper";
 buttons[2].textContent= "Scissors";
 
-for (let i=0; i < buttons.length; i++) {
-    buttons[i].textContent =  moveList[i];
-    buttons[i].style.display = "inline-block";
-}
 
-/**
- * Computes result of the game. returns victory message if move 1 wins.
- * @param  {Number}   move1  move 1
- * @param  {Number}   move2  move 2
- * @return {String}   result result of the game
- */
 
 function calcResult(move1, move2) {
     randomMove()
+    if(move1 === move2){
+        return tieMsg;
+    }else if((move1 === moveList[0] && move2 == moveList[2])||
+    (move1 === moveList[1] && move2 == moveList[0]) ||
+    (move1 === moveList[2] && move2 == moveList[1])){
+        return winMsg;
+    }else{
+        return loseMsg;
+    }
 }
+
 
 /**
  * @return {Number}   random number between 0 and 2
  */
 
 function randomMove() {
-
   return Math.floor(Math.random() * 3);
 }
 
@@ -45,7 +44,9 @@ function randomMove() {
 function startGame() {
     statusDisplay.textContent = 'Choose!';
 
-    
+    element.addEventListener(eventType,listener);
+    //chooses move 
+    const computerChoice = randomMove();
     return;
 }
 
